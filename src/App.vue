8aeb60bd1103f0fbb3f1b4a7871d0b3b91c4976d
Lamartio <lamarti.danny@gmail.com>
2025-11-26 10:52:33 +0000

    Make demo more interactive

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <v-app>
-    <v-app-bar color="primary" prominent title="App Store Demo"/>
-
     <v-main>
       <v-container>
         <v-row>
           <v-col cols="12">
             <v-card title="Welcome to ANT" text="Your project is set up and ready to go!">
-              <v-card-actions>
-                <v-btn color="primary" @click="increment">
+              <v-card-actions class="justify-center">
+                <v-btn color="primary" variant="tonal" @click="increment">
                   Count: {{ count }}
                 </v-btn>
               </v-card-actions>
@@ -21,15 +19,29 @@
 </template>
 
 <script setup lang="ts">
-import {ref, watch} from 'vue'
+import {onMounted, ref, watch} from 'vue'
 import {injectContext} from "./plugins/context.ts";
 import {useTheme} from "vuetify/framework";
 
-const { colorMode } = injectContext()
+const {colorMode, comms: {toolbar: {menu, title}}} = injectContext()
 const theme = useTheme()
 const count = ref(0)
 
 watch(colorMode.isDark, isDark => theme.change(isDark ? 'dark' : 'light'))
 
-const increment = () => count.value++
+onMounted(() => {
+
+  title.value = 'Welcome to the demo!'
+  menu.value = [
+    {
+      icon: "mdi-plus",
+      title: "Increment",
+      onClick: increment
+    }
+  ]
+})
+
+function increment() {
+  title.value = `Count: ${count.value++}`
+}
 </script>
